c26554c2efa3eaedbf9b1b7fdc828576fd124465
Psanjay-15 <sanjaypandere26@gmail.com>
2024-08-29 21:21:16 +0530

    #

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
             </p>
 
             <Link
-              to="https://drive.google.com/file/d/1zPyjR1tY1eia6jbACtecx8tpNzBK8Zvx/view"
+              to="https://drive.google.com/file/d/1YQnUbk9gmvkgmCjmqn1Q5xVsbjMwebK9/view?usp=sharing"
               target="blank"
             >
               <button className="mt-6 bg-white text-gray-900 font-semibold lg:font-extrabold lg:text-xl lg:py-3 py-2 px-4 lg:px-8 rounded-md hover:bg-gray-200 transition-colors">
